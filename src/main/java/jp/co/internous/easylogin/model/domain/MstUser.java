c5b4cdee7c4dc9a149b9266529cc4108e9827917
jtNao <ntoko10132gmail.com>
2024-04-24 11:00:39 +0900

    課題完成

---
+++ src/main/java/jp/co/internous/easylogin/model/domain/MstUser.java
@@ -0,0 +1,62 @@
+package jp.co.internous.easylogin.model.domain;
+
+public class MstUser {
+
+	private int id;
+	private String userName;
+	private String password;
+	private String fullName;
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPasswor(){
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getFullName() {
+		return fullName;
+	}
+	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
